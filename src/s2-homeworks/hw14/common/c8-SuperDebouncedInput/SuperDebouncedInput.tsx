6757468111e3972915c7e7c65963bcbdd24e7dad
Ustin4 <Mortyvp@yandex.by>
2023-08-31 13:04:59 +0300

    change HW14 replay

--- src/s2-homeworks/hw14/common/c8-SuperDebouncedInput/SuperDebouncedInput.tsx
+++ src/s2-homeworks/hw14/common/c8-SuperDebouncedInput/SuperDebouncedInput.tsx
@@ -26,21 +26,21 @@ const SuperDebouncedInput: React.FC<SuperDebouncedInputPropsType> = (
         ...restProps // все остальные пропсы попадут в объект restProps
     }
 ) => {
-    const [timerId, setTimerId] = useState<number | undefined>(undefined)
+    const [timerId, setTimerId] = useState<NodeJS.Timeout | undefined>(undefined)
 
     const onChangeTextCallback = (value: string) => {
         onChangeText?.(value)
 
         if (onDebouncedChange) {
-            // делает студент
             clearTimeout(timerId)
-
-            setTimeout(() => {
+            // делает студент
+            const timer = setTimeout(() => {
                 onDebouncedChange(value)
             }, 1500)
+
+            setTimerId(timer)
             // остановить предыдущий таймер
             // запустить новый на 1500ms, в котором вызовется функция
-
             //
         }
     }
